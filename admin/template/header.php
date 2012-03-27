fdd0a16107baa3f4605bde030385014bfe615be6
Markus Bach <markus@derpaderborner.de>
2012-03-27 19:23:01 +0200

    fixed header

--- admin/template/header.php
+++ admin/template/header.php
@@ -3,5 +3,7 @@
         <a href="<?php echo $this->url; ?>">back to your PocPoc</a>
         &nbsp;·&nbsp;
         <a href="<?php echo $this->url; ?>admin/?site=images">images</a>
+        &nbsp;·&nbsp;
+        <a href="<?php echo $this->url; ?>admin/?site=settings">settings</a>
     </div>
 </div>
