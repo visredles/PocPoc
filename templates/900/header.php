1b3c1ad18ce398b611cf6c9b56e637c93e140408
Markus Bach <markus@derpaderborner.de>
2012-05-20 10:57:02 +0200

    Twitter Button hinzugefügt

--- templates/900/header.php
+++ templates/900/header.php
@@ -1,1 +1,3 @@
-<div id="menu"><a href="<?php echo $this->url; ?>">Current</a> <a href="<?php echo $this->url; ?>random">Random</a> <a href="<?php echo $this->url; ?>archive">Archiv</a> <a href="<?php echo $this->url; ?>about">About</a></div>
+<div id="menu">
+<?php if(basename($this->file)=='show.php') echo '<a href="https://twitter.com/intent/tweet?text='.urlencode($this->title.' - '.$this->url.'pic/'.$this->pic['id']).'" target="_blank"><img style="float:left;" src="'.$this->url.'templates/900/images/twitter.png"></a>'; ?>
+<a href="<?php echo $this->url; ?>">Current</a> <a href="<?php echo $this->url; ?>random">Random</a> <a href="<?php echo $this->url; ?>archive">Archiv</a> <a href="<?php echo $this->url; ?>about">About</a></div>
