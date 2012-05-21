9187450fe41593deefa77ad1b12473d4bd7d77b1
Markus Bach <markus@derpaderborner.de>
2012-05-21 23:12:04 +0200

    RSS-Button im Menü (\_/) This is the commitbunny. (O.o) Copy commitbunny into your commit message to (> <) help him gain power over all humanity.

--- templates/900/header.php
+++ templates/900/header.php
@@ -1,3 +1,11 @@
 <div id="menu">
-<?php if(basename($this->file)=='show.php') echo '<a href="https://twitter.com/intent/tweet?text='.urlencode($this->title.' - '.$this->url.'pic/'.$this->pic['id']).'" target="_blank"><img style="float:left;" src="'.$this->url.'templates/900/images/twitter.png"></a>'; ?>
-<a href="<?php echo $this->url; ?>">Current</a> <a href="<?php echo $this->url; ?>random">Random</a> <a href="<?php echo $this->url; ?>archive">Archiv</a> <a href="<?php echo $this->url; ?>about">About</a></div>
+<a href="<?php echo $this->url; ?>rss" title="RSS-Feed"><img src="<?php echo $this->url; ?>templates/900/images/rss.png" alt="RSS-Logo"></a>
+<?php
+if(basename($this->file)=='show.php')
+	echo '<a href="https://twitter.com/intent/tweet?text='.urlencode($this->title.' - '.$this->url.'pic/'.$this->pic['id']).'" title="Bild zu diesem Link tweeten" target="_blank"><img alt="Twitter-Logo" src="'.$this->url.'templates/900/images/twitter.png"></a>';
+?>
+<a href="<?php echo $this->url; ?>" title="Aktuelles Bild">Current</a>
+<a href="<?php echo $this->url; ?>random" title="Zufälliges Bild">Random</a>
+<a href="<?php echo $this->url; ?>archive" title="Bilderarchiv">Archiv</a>
+<a href="<?php echo $this->url; ?>about" title="Impressum usw">About</a>
+</div>
