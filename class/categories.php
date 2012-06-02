e0827b741811d94ef54d08ff85666b95d999b8f1
Markus Bach <markus@derpaderborner.de>
2012-06-02 15:44:46 +0200

    Kategorie-Klasse hinzugefügt und SQL up-to-date gebracht. (\_/) This is the commitbunny. (O.o) Copy commitbunny into your commit message to (> <) help him gain power over all humanity.

---
+++ class/categories.php
@@ -0,0 +1,44 @@
+<?php
+/* This software was written by Markus Bach <markus@derpaderborner.de>
+ * It is licensed as beerware. So drink up and get me a beer!
+ * Just kidding. Have fun with this piece of crap.
+ */
+
+require_once('database.php');
+
+class Categories {
+	private $db;
+	function Categories() {
+		$this->db = new Database();
+	}
+	function getCats($picid,$what='*') {
+		$picide = (int) $picid;
+		return $this->db->query('SELECT prefix_categories.'.$what.' FROM prefix_categories, prefix_catassoc WHERE prefix_categories.id = prefix_catassoc.catid AND prefix_catassoc.picid = '.$picid);
+	}
+	function getAllCats() {
+		return $this->db->query('SELECT * FROM prefix_categories');
+	}
+	function newCat($title, $descr) {
+		return $this->db->query('INSERT INTO prefix_categories (`title`, `description`) VALUES ("'.$title.'", "'.$descr.'");');
+	}
+	function pic2cat($picid,$catid) {
+		return $this->db->query('INSERT INTO prefix_catassoc (`picid`, `catid`) VALUES ('.$picid.', '.$catid.');');
+	}
+	function del($id){
+		return $this->db->query('DELETE FROM prefix_categories WHERE `id`='.$id);
+	}
+	function del_assoc($picid,$catid) {
+		return $this->db->query('DELETE FROM prefix_catassoc WHERE `picid`='.$picid.' AND `catid`='.$catid);
+	}
+
+	public function getHash() {
+		$str='';
+		foreach($this->args as $arg){
+			if(is_string($arg)) $str.=$arg;
+			elseif(is_numeric($arg)) $str.=$arg;
+			elseif(is_object($arg)) $str.=$arg->getHash();
+		}
+		return sha1($str);
+	}
+}
+?>
--- fotos_pics.sql
+++ fotos_pics.sql
@@ -5,6 +5,34 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+CREATE TABLE IF NOT EXISTS `fotos_catassoc` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `catid` int(11) NOT NULL,
+  `picid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `catid` (`catid`),
+  KEY `picid` (`picid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `fotos_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `fotos_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picid` int(11) NOT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `homepage` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
 
 CREATE TABLE IF NOT EXISTS `fotos_pics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -13,4 +41,4 @@ CREATE TABLE IF NOT EXISTS `fotos_pics` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `image` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
